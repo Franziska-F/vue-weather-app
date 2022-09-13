9c339c882e3ae204861dfc02d1dcd05f15e20817
Franziska-F <f.fouchs@gmail.com>
2022-09-13 11:54:07 +0200

    Add dotenv

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,7 +1,19 @@
 <script setup></script>
 
 <template>
-  <main></main>
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input
+        type="text"
+        v-model="searchQuery"
+        placeholder="Search"
+        class="py-2 px-1 w-full bg-transparent border-b focus: border-blue-700 focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+    </div>
+  </main>
 </template>
 
-
+<script setup>
+import { ref } from "vue";
+const searchQuery = ref("");
+</script>
